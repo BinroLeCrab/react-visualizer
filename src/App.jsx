71959d443f853bb5c81b8fd274c1ca5e8e3f3a95
Robin Vigier / B!nro <rob.vigier@gmail.com>
2025-05-10 17:19:14 +0200

    💄 picker

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <>
       <Landing />
-      {/* <Dropzone /> */}
+      <Dropzone /> 
       <Picker />
       <Tracks />
       <Canvas />
--- src/components/Picker/Picker.jsx
+++ src/components/Picker/Picker.jsx
@@ -4,23 +4,29 @@ import s from "./Picker.module.scss";
 
 const VISUALIZERS = [
     {
+        name: "CD",
+        icon: "icon/cd-box.svg",
+        index: 5,
+    }, {
         name: "Cover",
+        icon: "icon/waves.svg",
         index: 0,
     }, {
         name: "Line",
+        icon: "icon/waveform.svg",
         index: 1,
     }, {
         name: "Board",
+        icon: "icon/equalizer.svg",
         index: 2,
     }, {
         name: "Logo Iut",
+        icon: "icon/univ-eiffel.svg",
         index: 3,
     }, {
         name: "Cube",
+        icon: "icon/cube.svg",
         index: 4,
-    }, {
-        name: "CD",
-        index: 5,
     }
 ];
 
@@ -35,16 +41,25 @@ const Picker = () => {
 
     return (
         <div className={s.picker}>
-            {VISUALIZERS.map( (visualizer) => (
-                <span
+            {VISUALIZERS.map((visualizer) => (
+                <div
                     key={visualizer.name}
-                    className={`${current === visualizer.index ? s.current : ""}`}
+                    className={`${s.item} ${current === visualizer.index ? s.current : ""}`}
                     onClick={() => pickVisualizer(visualizer.index)}
                 >
-                    {visualizer.name}
-                </span>
-            ))}
-        </div>
+                    <span
+                        className={s.name}>
+                        {visualizer.name}
+                    </span>
+                    <img
+                        src={visualizer.icon}
+                        alt={visualizer.name}
+                        className={s.icon}
+                    />
+                </div>
+            ))
+            }
+        </div >
     );
 };
 
