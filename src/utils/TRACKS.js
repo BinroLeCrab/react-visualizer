56e1fbcaa19bf4479c5c012f84ff4be2958033c0
Robin Vigier / B!nro <rob.vigier@gmail.com>
2025-05-12 00:15:17 +0200

    🍱 new music

--- src/utils/TRACKS.js
+++ src/utils/TRACKS.js
@@ -1,11 +1,17 @@
-import track01 from "/music/WetLeg-Daisy.mp3";
-import track02 from "/music/WetLeg-ItsAShame.mp3";
-import track03 from "/music/WetLeg-ItsNotFun.mp3";
+import Daisy from "/music/WetLeg-Daisy.mp3";
+import ItsAShame from "/music/WetLeg-ItsAShame.mp3";
+import ItsNotFun from "/music/WetLeg-ItsNotFun.mp3";
+import HighFinesHerbes from "/music/Bienvenue_dans_High_Fines_Herbes.mp3";
+import PrimoPiatto from "/music/Primo_Piatto.mp3";
+import ChaiseLongue from "/music/WetLeg-Chaise_Longue.mp3";
 
 const TRACKS = [
-    { id: 1, name: "Daisy", artist: "Wet Leg", path: track01 },
-    { id: 2, name: "It's A Shame", artist: "Wet Leg", path: track02 },
-    { id: 3, name: "It's Not Fun", artist:"Wet Leg", path: track03 }
+    { id: 1, name: "Daisy", artist: "Wet Leg", path: Daisy },
+    { id: 2, name: "It's A Shame", artist: "Wet Leg", path: ItsAShame },
+    { id: 3, name: "It's Not Fun", artist:"Wet Leg", path: ItsNotFun },
+    { id: 4, name: "Chaise Longue", artist: "Wet Leg", path: ChaiseLongue },
+    { id: 5, name: "Bienvenue dans High & Fines Herbes", path: HighFinesHerbes },
+    { id: 6, name: "Primo Piatto", path: PrimoPiatto },
 ];
 
 export default TRACKS;
